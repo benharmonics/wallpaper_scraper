a6960232ed39afc32c931b1fbeba8dcab7cd8dc0
benharmonics <benjamin.jiron+git@gmail.com>
2022-05-25 15:51:25 -0600

    Tolerance is static, so should only check it once

--- src/lib.rs
+++ src/lib.rs
@@ -27,20 +27,27 @@ fn scrape_dir(path: &Path, args: &ArgMatches) -> io::Result<()> {
         .collect::<Result<Vec<PathBuf>, _>>()
         .unwrap_or_default();
     pathbufs.retain(|buf| image_filetypes.contains(&buf.extension().unwrap_or_default()));
-    // Scraping suitable images - that is, copying them into a new directory.
+    // Getting argument values
     let output_dir = Path::new(args.value_of("output").unwrap());
     if !output_dir.exists() {
         fs::create_dir_all(&output_dir)?;
     }
+    let tolerance = match args.value_of("tolerance").unwrap() {
+        "high" => 0.22,
+        "med" => 0.15,
+        "low" => 0.08,
+        _ => 0.15,
+    };
+    // Scraping suitable images - that is, copying them into a new directory.
     for buf in &pathbufs {
-        if !image_is_suitable(buf.as_path(), args) { continue; }
+        if !image_is_suitable(buf.as_path(), tolerance) { continue; }
         fs::copy(buf, output_dir.join(buf.file_name().unwrap()))?;
     }
     Ok(())
 }
 
 // Check if image is HD and within an acceptable tolerance of a popular aspect ratio (16:9 or 4:3)
-fn image_is_suitable(path: &Path, args: &ArgMatches) -> bool {
+fn image_is_suitable(path: &Path, tolerance: f64) -> bool {
     let (width, height) = image::image_dimensions(path).unwrap_or_default();
     if width < 1920 || height < 1080 {
         return false
@@ -48,12 +55,6 @@ fn image_is_suitable(path: &Path, args: &ArgMatches) -> bool {
     let aspect_ratio = width as f64 / height as f64;
     let r1 = 16.0 / 9.0;
     let r2 = 4.0 / 3.0;
-    let tolerance = match args.value_of("tolerance").unwrap() {
-        "high" => 0.22,
-        "med" => 0.15,
-        "low" => 0.08,
-        _ => 0.15,
-    };
     if (aspect_ratio - r1).abs() / r1 > tolerance && (aspect_ratio - r2).abs() / r2 > tolerance {
         return false
     }

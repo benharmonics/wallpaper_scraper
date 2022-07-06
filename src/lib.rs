fd5ed2eb159c1fe080037eb200574ddd491f5f1f
benharmonics <benjamin.jiron+git@gmail.com>
2022-07-06 14:00:18 -0600

    Refactor the way extensions are handled + comments

--- src/lib.rs
+++ src/lib.rs
@@ -20,22 +20,27 @@ pub fn run(args: ArgMatches) -> io::Result<()> {
 
 fn scrape_dir(path: &Path, args: &ArgMatches) -> io::Result<()> {
     // Gathering PathBufs for items in given directory, then retaining just the image files for scraping
-    let image_filetypes = ["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"].map(|s| OsStr::new(s));
+    let image_filetypes = ["jpg", "jpeg", "png", "webp"].map(|s| OsStr::new(s));
     let mut pathbufs = fs::read_dir(path)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<PathBuf>, _>>()
         .unwrap_or_default();
-    pathbufs.retain(|buf| image_filetypes.contains(&buf.extension().unwrap_or_default()));
+    pathbufs.retain(|buf| {
+        let extension = buf.extension().unwrap_or_default().to_ascii_lowercase();
+        image_filetypes.contains(&extension.as_os_str())
+    });
     // Getting argument values
     let output_dir = Path::new(args.value_of("output").unwrap());
     if !output_dir.exists() {
         fs::create_dir_all(&output_dir)?;
     }
+    // We assume your monitor is 16:9 but you can set it to 4:3 as well
     let aspect_ratio = match args.value_of("aspect ratio").unwrap() {
         "16x9" => 16.0 / 9.0,
         "4x3" => 4.0 / 3.0,
         _ => unreachable!(),
     };
+    // Kind of arbitrary values. They were found by manually testing a lot of different size images.
     let tolerance = match args.value_of("tolerance").unwrap() {
         "high" => 0.22,
         "med" => 0.15,
@@ -47,11 +52,10 @@ fn scrape_dir(path: &Path, args: &ArgMatches) -> io::Result<()> {
         if !image_is_suitable(path.as_path(), aspect_ratio, tolerance) {
             continue;
         }
-        let path = path
-            .file_name()
-            .unwrap_or(OsStr::new(stringify!(_i)));
+        let path = path.file_name().unwrap_or(OsStr::new(stringify!(_i)));
         fs::copy(path, output_dir.join(path))?;
     }
+
     Ok(())
 }
 
@@ -64,12 +68,15 @@ fn image_is_suitable(path: &Path, aspect_ratio: f64, tolerance: f64) -> bool {
             (0, 0)
         }
     };
+    // size check - images must be HD
     if width < 1920 || height < 1080 {
         return false;
     }
+    // aspect ratio check - images should be 16:9 (with some wiggle room)
     let image_aspect_ratio = width as f64 / height as f64;
     if (image_aspect_ratio - aspect_ratio).abs() / aspect_ratio > tolerance {
         return false;
     }
+
     true
 }
